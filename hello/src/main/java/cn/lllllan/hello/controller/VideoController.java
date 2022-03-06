45a23b6f7cf3c3c046f12029665a052ec5223e97
lllllan <342310798@qq.com>
2022-03-07 00:55:20 +0800

    getMapping & postMapping

--- hello/src/main/java/cn/lllllan/hello/controller/VideoController.java
+++ hello/src/main/java/cn/lllllan/hello/controller/VideoController.java
@@ -1,5 +1,7 @@
 package cn.lllllan.hello.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,22 @@ public class VideoController {
 
     @RequestMapping("list")
     public Object list() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "面试专题");
+        map.put("2", "SpringCloud");
+        return map;
+    }
+
+    @PostMapping("postList")
+    public Object postList() {
+        Map<String, String> map = new HashMap<>();
+        map.put("1", "面试专题");
+        map.put("2", "SpringCloud");
+        return map;
+    }
+
+    @GetMapping("getList")
+    public Object getList() {
         Map<String, String> map = new HashMap<>();
         map.put("1", "面试专题");
         map.put("2", "SpringCloud");
