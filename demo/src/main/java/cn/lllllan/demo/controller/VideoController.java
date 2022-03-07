6964133d170f9cf9ba67aa29305ef3a3a772641c
lllllan <342310798@qq.com>
2022-03-07 11:21:00 +0800

    add JsonData

--- demo/src/main/java/cn/lllllan/demo/controller/VideoController.java
+++ demo/src/main/java/cn/lllllan/demo/controller/VideoController.java
@@ -1,13 +1,11 @@
 package cn.lllllan.demo.controller;
 
-import cn.lllllan.demo.domain.Video;
 import cn.lllllan.demo.service.VideoService;
+import cn.lllllan.demo.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/video")
@@ -17,7 +15,7 @@ public class VideoController {
     private VideoService videoService;
 
     @GetMapping("listVideo")
-    public List<Video> listVideo() {
-        return videoService.listVideo();
+    public Object listVideo() {
+        return JsonData.buildSuccess(videoService.listVideo());
     }
 }
