1119711ca44526b656efb450fc8fee9fdbc66616
lllllan <342310798@qq.com>
2022-03-07 10:42:02 +0800

    add mapper.videoMapper

--- demo/src/main/java/cn/lllllan/demo/mapper/VideoMapper.java
+++ demo/src/main/java/cn/lllllan/demo/mapper/VideoMapper.java
@@ -1,4 +1,18 @@
 package cn.lllllan.demo.mapper;
 
+import cn.lllllan.demo.domain.Video;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class VideoMapper {
+
+    private static Map<Integer, Video> videoMap = new HashMap<>();
+
+    static {
+        videoMap.put(1, new Video(1, "Java基础课程"));
+        videoMap.put(1, new Video(2, "SpringBoot"));
+        videoMap.put(1, new Video(3, "SpringMVC"));
+        videoMap.put(1, new Video(4, "SpringCloud"));
+    }
 }
