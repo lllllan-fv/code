e4547933f3d98e913d35bee48bc481fa83dd894e
lllllan <342310798@qq.com>
2022-03-07 23:29:52 +0800

    全局异常处理 - 跳转页面

--- demo/src/main/java/cn/lllllan/demo/handler/CustomExtHandler.java
+++ demo/src/main/java/cn/lllllan/demo/handler/CustomExtHandler.java
@@ -1,19 +1,28 @@
 package cn.lllllan.demo.handler;
 
-import cn.lllllan.demo.utils.JsonData;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * 标记这是一个异常处理类
  */
-@RestControllerAdvice
+//@RestControllerAdvice
+@ControllerAdvice
 public class CustomExtHandler {
+
+//    @ExceptionHandler(value = Exception.class)
+//    Object handlerException(Exception e, HttpServletRequest request) {
+//        return JsonData.buildError(2, "服务端出错");
+//    }
 
     @ExceptionHandler(value = Exception.class)
     Object handlerException(Exception e, HttpServletRequest request) {
-        return JsonData.buildError(2, "服务端出错");
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error.html");
+        modelAndView.addObject("msg", e.getMessage());
+        return modelAndView;
     }
 }
