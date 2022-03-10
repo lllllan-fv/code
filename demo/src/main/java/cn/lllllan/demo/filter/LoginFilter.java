2fe6f62200459c8eead1fb3ebd3fa8d90ba82fce
lllllan <342310798@qq.com>
2022-03-10 15:28:50 +0800

    过滤器：打印出错误

--- demo/src/main/java/cn/lllllan/demo/filter/LoginFilter.java
+++ demo/src/main/java/cn/lllllan/demo/filter/LoginFilter.java
@@ -2,6 +2,8 @@ package cn.lllllan.demo.filter;
 
 import cn.lllllan.demo.domain.User;
 import cn.lllllan.demo.service.impl.UserServiceImpl;
+import cn.lllllan.demo.utils.JsonData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.*;
@@ -9,9 +11,12 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebFilter(urlPatterns = "/api/v1/pri/*", filterName = "LoginFilter")
 public class LoginFilter implements Filter {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -31,11 +36,29 @@ public class LoginFilter implements Filter {
         }
 
         if (StringUtils.isEmpty(token)) {
+            JsonData jsonData = JsonData.buildError(-3, "未登录");
+            String jsonStr = objectMapper.writeValueAsString(jsonData);
+            renderJson(httpServletResponse, jsonStr);
         } else {
             User user = UserServiceImpl.sessionMap.get(token);
             if (user != null) {
                 filterChain.doFilter(servletRequest, servletResponse);
+            } else {
+                JsonData jsonData = JsonData.buildError(-2, "登录失败，token无效");
+                String jsonStr = objectMapper.writeValueAsString(jsonData);
+                renderJson(httpServletResponse, jsonStr);
             }
+        }
+    }
+
+    private void renderJson(HttpServletResponse response, String json) {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        try (PrintWriter printWriter = response.getWriter()) {
+            printWriter.println(json);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
