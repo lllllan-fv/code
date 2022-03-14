ada0db16e2d40f8ba263344f971cf3eb3b3f3ccc
lllllan <342310798@qq.com>
2022-03-14 13:16:06 +0800

    创建新项目 & 添加依赖

---
+++ springboot-learn/src/test/java/cn/lllllan/springbootlearn/SpringbootLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lllllan.springbootlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
