a3b2aba98e49eb9e9b61453ad9beedc209cb98cf
lllllan <342310798@qq.com>
2022-03-14 14:51:12 +0800

    修改返回

--- springboot-learn/src/main/java/cn/lllllan/springbootlearn/controller/VideoController.java
+++ springboot-learn/src/main/java/cn/lllllan/springbootlearn/controller/VideoController.java
@@ -1,12 +1,16 @@
 package cn.lllllan.springbootlearn.controller;
 
+import cn.lllllan.springbootlearn.domain.Video;
 import cn.lllllan.springbootlearn.service.VideoService;
+import cn.lllllan.springbootlearn.utils.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/video")
+@RequestMapping("/api/v1/pub/video")
 public class VideoController {
 
     @Autowired
@@ -14,6 +18,7 @@ public class VideoController {
 
     @RequestMapping("list")
     public Object listVideo() {
-        return videoService.listVideo();
+        List<Video> videos = videoService.listVideo();
+        return JsonData.buildSuccess(videos);
     }
 }
