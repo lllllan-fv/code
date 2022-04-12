1d9f2737db96eff4b0fd277e728e955baac75d43
lllllan <342310798@qq.com>
2022-04-12 15:43:12 +0800

    treemap

---
+++ CollectionSource/src/map/TreeMapDemo.java
@@ -0,0 +1,9 @@
+package map;
+
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+    }
+}
