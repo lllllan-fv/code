1eb4e35299a912d0e6d2ddb8721c6f91c509906f
lllllan <342310798@qq.com>
2022-04-12 14:36:56 +0800

    priorityqueue

---
+++ CollectionSource/src/PriorityQueueDemo.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> Q = new PriorityQueue<>();
+    }
+
+    /**
+     * 默认初始容量 11
+     */
+
+    /**
+     * 扩容
+     * - capacity <  64: capacity * 2 + 2
+     * - capacity >= 64: capacity * 1.5
+     */
+}
