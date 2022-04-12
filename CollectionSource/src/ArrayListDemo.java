ab73ddafff0ef257c0f8c619b742536c1bab6dfa
lllllan <342310798@qq.com>
2022-04-12 09:04:58 +0800

    arraylist 扩容

---
+++ CollectionSource/src/ArrayListDemo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+    }
+
+    /**
+     * 每次扩容 0.5 倍
+     * int newCapacity = oldCapacity + (oldCapacity >> 1);
+     */
+}
