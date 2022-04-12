3e4433085f5d0ac60fb8f08fefc6173a1f1dde6f
lllllan <342310798@qq.com>
2022-04-12 09:03:37 +0800

    vector 扩容

--- CollectionSource/src/VectorDemo.java
+++ CollectionSource/src/VectorDemo.java
@@ -6,4 +6,9 @@ public class VectorDemo {
     public static void main(String[] args) {
         List<Integer> list = new Vector<>();
     }
+
+    /**
+     * 指定的 capacityIncrement 是增量，如果指定过增量，每次按照该大小进行扩容，否则每次扩容一倍
+     * int newCapacity = oldCapacity + ((capacityIncrement > 0) ? capacityIncrement : oldCapacity);
+     */
 }
