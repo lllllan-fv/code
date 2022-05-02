438a722bcc5963ffe285a30e5ce7e6355c5009ae
lllllan <342310798@qq.com>
2022-05-02 10:36:47 +0800

    delete

--- CollectionSource/src/map/TreeMapDemo.java
+++
@@ -1,9 +0,0 @@
-package map;
-
-import java.util.TreeMap;
-
-public class TreeMapDemo {
-    public static void main(String[] args) {
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-    }
-}
--- demo/src/main/java/cn/lllllan/demo/service/VideoService.java
+++
@@ -1,9 +0,0 @@
-package cn.lllllan.demo.service;
-
-import cn.lllllan.demo.domain.Video;
-
-import java.util.List;
-
-public interface VideoService {
-    public List<Video> listVideo();
-}
